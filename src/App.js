72517c4d1a8566bf0b7d9d66afac7672ba802d10
tamimkarim <67522154+tamimkarim@users.noreply.github.com>
2020-10-07 03:54:55 +0600

    first commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+import ReducerCount from './components/ReducerCount/ReducerCount';
+
+function App() {
+  return (
+    <div className="App">
+      <ReducerCount></ReducerCount>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/ReducerCount/ReducerCount.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const ReducerCount = () => {
+    return (
+        <div>
+            <h2>This is Reducer Count</h2>
+        </div>
+    );
+};
+
+export default ReducerCount;
